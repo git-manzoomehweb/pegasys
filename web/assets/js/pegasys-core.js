9ebb8e02adfa6a8761c7a6f0f2f38a16df7b196e
kmg.d102 <kmg.d102@gmail.com>
2025-04-20 07:28:23 -0700

    js menu mobile updated

--- web/assets/js/pegasys-core.js
+++ web/assets/js/pegasys-core.js
@@ -6,19 +6,23 @@ if (window.innerWidth >= 1024) {
   headerMenuClose.addEventListener("click", function () {
     headerMenu.style.visibility = "hidden";
     headerMenu.style.opacity = "0";
-    // headerMenu.style.display = "none";
+    document.body.classList.remove("overflow-hidden");
   });
+
   bars3.addEventListener("click", function () {
     headerMenu.style.visibility = "visible";
     headerMenu.style.opacity = "1";
-    // headerMenu.style.display = "block";
+    document.body.classList.add("overflow-hidden");
   });
 } else {
   headerMenuClose.addEventListener("click", function () {
-    headerMenu.style.transform = "translateX(1024px)";
+    headerMenu.style.transform = "translateX(-1024px)";
+    document.body.classList.remove("overflow-hidden");
   });
+
   bars3.addEventListener("click", function () {
     headerMenu.style.transform = "translateX(0)";
+    document.body.classList.add("overflow-hidden");
   });
 }
 document.addEventListener("DOMContentLoaded", function () {
